5c66b1b48200e735196e989407d6e6b7ebd8be75
Morpheus <wangmykj2001@gmail.com>
2026-01-05 14:46:26 +0900

    Add dqt_json.py
    All JSON handling is now moved from day_quality_tracker.py to dqt_json.py

---
+++ dqt_json.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from day_quality_tracker import DayQualityTracker
+
+
+class DQTJSON:
+    """Manage Day Quality Tracker JSON contents handling"""
+
+    def __init__(self, dqt: DayQualityTracker):
+        self.dqt = dqt
+        self.date_format = self.dqt.date_format
+
+        self.json_path = Path(__file__).resolve().parent / 'dq_ratings.json'
+
+        self.saved_ratings = self._load_json()
+
+    def update(self):
+        """Dump updated `saved_ratings` dict to JSON file."""
+        if not self.json_path.exists():
+            self.json_path.touch()
+
+        with open(self.json_path, 'w') as json_file:
+            json.dump(self.saved_ratings, json_file, indent=4)
+
+    def _load_json(self) -> dict:
+        """Load JSON file contents and return dict."""
+        if not self.json_path.exists():
+            self.json_path.touch()
+
+        if not self.json_path.read_text():
+            return {}
+        with open(self.json_path, 'r') as file:
+            contents = json.load(file)
+        return {
+            k: float(v) for k, v in contents.items()
+        }
