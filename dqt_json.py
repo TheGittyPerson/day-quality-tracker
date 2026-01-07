a0a66db4342963f7b68697cb3912808f46a1efa7
Morpheus <wangmykj2001@gmail.com>
2026-01-07 11:52:21 +0900

    Add `touch` method to DQTJSON
    This method creates the JSON file if it doesn't exist, but if the old file name from DQT-4, 'dq_ratings.json', is found, it is automatically renamed to 'dq_logs.json'.

--- dqt_json.py
+++ dqt_json.py
@@ -13,9 +13,21 @@ class DQTJSON:
         self.dqt = dqt
         self.date_format = self.dqt.date_format
 
-        self.json_path = Path(__file__).resolve().parent / 'dq_logs.json'
+        self.parent_dir = Path(__file__).resolve().parent
+        self.json_name = 'dq_logs.json'
+        self.json_name_pre5 = 'dq_ratings.json'
+        self.json_path = self.parent_dir / self.json_name
+        self.json_path_pre5 = self.parent_dir / self.json_name_pre5
 
         self.saved_ratings = self._load_json()
+
+    def touch(self) -> None:
+        """Check if JSON file exists."""
+        if not self.json_path.exists():
+            if self.json_path_pre5.exists():
+                self.json_path_pre5.rename(self.json_name)
+            else:
+                self.json_path.touch()
 
     def update(self):
         """Dump updated `saved_ratings` dict to JSON file."""
