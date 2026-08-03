e109a2a28a8ab97b07d7a1507a96042e5ab7a3f2
Morpheus <167074500+TheGittyPerson@users.noreply.github.com>
2026-08-03 11:50:57 +0900

    Move imports in `__init__.py` after metadata
    Signed-off-by: Morpheus <167074500+TheGittyPerson@users.noreply.github.com>

--- dqt/__init__.py
+++ dqt/__init__.py
@@ -1,11 +1,3 @@
-from .tracker import Tracker
-from .manager import Manager
-from .json_manager import JSONManager
-from .graph import Graph
-from .stats import Stats
-from .settings_manager import SettingsManager
-from . import ui_utils, shortcut_creator
-
 __name__ = "dqt"
 __package__ = "dqt"
 __master_version__ = 5
@@ -13,6 +5,14 @@ __version__ = "v1.4.1"
 __author__ = "Morpheus"
 
 _REPO_URL = "https://github.com/TheGittyPerson/day-quality-tracker"
+
+from .tracker import Tracker
+from .manager import Manager
+from .json_manager import JSONManager
+from .graph import Graph
+from .stats import Stats
+from .settings_manager import SettingsManager
+from . import ui_utils, shortcut_creator
 
 __all__ = [
     "Tracker", "Manager", "JSONManager", "Graph", "Stats", "SettingsManager"
